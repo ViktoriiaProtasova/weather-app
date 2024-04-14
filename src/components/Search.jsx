2679668716d8a20c93dfcbec423374ca3403390e
Viktoriia Protasova <viktorka.protasova@gmail.com>
2024-04-14 22:10:31 +0300

    loader was added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { MagnifyingGlass } from 'react-loader-spinner';
 
 export default function Search() {
-  const [city, setCity] = useState(null);
+  const [city, setCity] = useState('');
   const [weather, setWeather] = useState(false);
   const [temperature, setTemperature] = useState(null);
   const [description, setDescription] = useState(null);
@@ -11,6 +12,7 @@ export default function Search() {
   const [icon, setIcon] = useState(null);
   const [error, setError] = useState(false);
   const [message, setMessage] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const units = 'metric';
   const apiKey = '4e2ef63817950c7df54e72f923dae8f1';
@@ -19,12 +21,13 @@ export default function Search() {
   const handleSubmit = e => {
     e.preventDefault();
     if (city) {
-      const queryCity = e.target.elements.search.value;
+      const queryCity = city;
       let capitalizedCity = `${queryCity[0].toUpperCase()}${queryCity.slice(
         1
       )}`;
       setCity(capitalizedCity);
       setWeather(true);
+      setIsLoading(true);
 
       axios
         .get(url)
@@ -35,12 +38,15 @@ export default function Search() {
           setHumidity(response.data.main.humidity);
           setWind(response.data.wind.speed);
           setIcon(iconUrl);
+          setIsLoading(false);
         })
         .catch(error => {
           console.log(error.message);
           setError(true);
+          setIsLoading(false);
         });
     } else {
+      setIsLoading(false);
       setMessage(true);
     }
   };
@@ -50,39 +56,55 @@ export default function Search() {
   };
 
   const handleReset = () => {
-    setCity(null);
+    setCity('');
     setWeather(false);
     setError(false);
     setMessage(false);
+    setIsLoading(false);
   };
 
   return (
     <>
-      <form className="Search" onSubmit={handleSubmit}>
-        <input
-          type="search"
-          name="search"
-          placeholder="Type a city..."
-          onChange={handleChange}
-          onInput={handleReset}
+      {isLoading ? (
+        <MagnifyingGlass
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="magnifying-glass-loading"
+          wrapperStyle={{}}
+          wrapperClass="magnifying-glass-wrapper"
+          glassColor="#f2f2f1"
+          color="#48484a"
         />
-        <input type="submit" value="Search" />
-      </form>
-      <br />
-      {message && <b>Please type a city...</b>}
-      {error && <b>Please type the correct city...</b>}
-      {weather && !error && (
+      ) : (
         <>
-          <b>Current weather in {city}:</b>
-          <ul className="weatherList">
-            <li>Temperature: {Math.round(temperature)}°C</li>
-            <li>Description: {description}</li>
-            <li>Humidity: {humidity}%</li>
-            <li>Wind: {wind}km/h</li>
-            <li>
-              <img src={icon} alt="Weather icon" />
-            </li>
-          </ul>
+          <form className="Search" onSubmit={handleSubmit}>
+            <input
+              type="search"
+              name="search"
+              placeholder="Type a city..."
+              onChange={handleChange}
+              onInput={handleReset}
+            />
+            <input type="submit" value="Search" />
+          </form>
+          <br />
+          {message && <b>Please type a city...</b>}
+          {error && <b>Please type the correct city...</b>}
+          {weather && !error && (
+            <>
+              <b>Current weather in {city}:</b>
+              <ul className="weatherList">
+                <li>Temperature: {Math.round(temperature)}°C</li>
+                <li>Description: {description}</li>
+                <li>Humidity: {humidity}%</li>
+                <li>Wind: {wind}km/h</li>
+                <li>
+                  <img src={icon} alt="Weather icon" />
+                </li>
+              </ul>
+            </>
+          )}
         </>
       )}
     </>
