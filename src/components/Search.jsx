af0e2bb5e99dc254acef5d2756850687d0c0ec6c
Viktoriia Protasova <viktorka.protasova@gmail.com>
2024-04-14 20:08:23 +0300

    Search engine was updated

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,45 +1,90 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function Search() {
-  const [city, setCity] = useState('');
+  const [city, setCity] = useState(null);
   const [weather, setWeather] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+  const [wind, setWind] = useState(null);
+  const [icon, setIcon] = useState(null);
+  const [error, setError] = useState(false);
+  const [message, setMessage] = useState(false);
+
+  const units = 'metric';
+  const apiKey = '4e2ef63817950c7df54e72f923dae8f1';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
   const handleSubmit = e => {
     e.preventDefault();
-
-    const city = e.target.elements.search.value;
-    let capitalizedCity = `${city[0].toUpperCase()}${city.slice(1)}`;
-
-    setCity(capitalizedCity);
-
     if (city) {
+      const queryCity = e.target.elements.search.value;
+      let capitalizedCity = `${queryCity[0].toUpperCase()}${queryCity.slice(
+        1
+      )}`;
+      setCity(capitalizedCity);
       setWeather(true);
+
+      axios
+        .get(url)
+        .then(function (response) {
+          const iconUrl = `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+          setTemperature(response.data.main.temp);
+          setDescription(response.data.weather[0].description);
+          setHumidity(response.data.main.humidity);
+          setWind(response.data.wind.speed);
+          setIcon(iconUrl);
+        })
+        .catch(error => {
+          console.log(error.message);
+          setError(true);
+        });
+    } else {
+      setMessage(true);
     }
   };
 
+  const handleChange = e => {
+    setCity(e.target.value);
+  };
+
   const handleReset = () => {
-    setCity('');
+    setCity(null);
     setWeather(false);
+    setError(false);
+    setMessage(false);
   };
 
   return (
     <>
-      <h1>Weather Search Engine</h1>
       <form className="Search" onSubmit={handleSubmit}>
         <input
           type="search"
           name="search"
           placeholder="Type a city..."
+          onChange={handleChange}
           onInput={handleReset}
         />
         <input type="submit" value="Search" />
-        {weather && (
-          <p>
-            It is currently {Math.round(Math.random() * (30 - 0 + 1) + 0)}°C in{' '}
-            {city}
-          </p>
-        )}
       </form>
+      <br />
+      {message && <b>Please type a city...</b>}
+      {error && <b>Please type the correct city...</b>}
+      {weather && !error && (
+        <>
+          <b>Current weather in {city}:</b>
+          <ul className="weatherList">
+            <li>Temperature: {Math.round(temperature)}°C</li>
+            <li>Description: {description}</li>
+            <li>Humidity: {humidity}%</li>
+            <li>Wind: {wind}km/h</li>
+            <li>
+              <img src={icon} alt="Weather icon" />
+            </li>
+          </ul>
+        </>
+      )}
     </>
   );
 }
