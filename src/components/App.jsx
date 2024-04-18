ca1ef1ab5c2735a33319000a4da302d115ea4f59
Viktoriia Protasova <viktorka.protasova@gmail.com>
2024-04-18 13:03:27 +0300

    project ready to be deployed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,10 @@
-// import Sky from '../components/Sky/Sky';
-import Dropdown from './Dropdown/Dropdown';
 import Search from './Search';
 
 export const App = () => {
   return (
     <div style={{ textAlign: 'center' }}>
-      {/* <h1>Web site for a forecast of weather</h1> */}
-      {/* <Dropdown /> */}
+      <h1>Web site for a forecast of weather</h1>
       <Search />
-      {/* <Sky initialValue={1} /> */}
     </div>
   );
 };
--- src/components/Sky.js/Value.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Value = ({ value }) => {
-  return <p>{value}</p>;
-};
-
-export default Value;
