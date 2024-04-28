a74fe92c8893b13c991a43eefe0bf8049f38fd82
Viktoriia Protasova <viktorka.protasova@gmail.com>
2024-04-28 22:52:04 +0300

    Create Footer.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      This project was coded by{' '}
+      <a href="https://www.linkedin.com/in/vprotasova/">Viktoriia Protasova</a>{' '}
+      and is open-sourced on{' '}
+      <a href="https://github.com/ViktoriiaProtasova/weather-app">GitHub</a> and
+      hosted on <a href="https://weatherprovide.netlify.app/">Netlify</a>
+    </div>
+  );
+}
