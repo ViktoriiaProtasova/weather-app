0ea18740681db0f79069f83d690d7f19825ebdc1
Viktoriia Protasova <viktorka.protasova@gmail.com>
2024-05-07 18:09:01 +0300

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,10 +5,20 @@ export default function Footer() {
   return (
     <div className="Footer">
       This project was coded by{' '}
-      <a href="https://www.linkedin.com/in/vprotasova/">Viktoriia Protasova</a>{' '}
+      <a href="https://www.linkedin.com/in/vprotasova/" target="_blank">
+        Viktoriia Protasova
+      </a>{' '}
       and is open-sourced on{' '}
-      <a href="https://github.com/ViktoriiaProtasova/weather-app">GitHub</a> and
-      hosted on <a href="https://weatherprovide.netlify.app/">Netlify</a>
+      <a
+        href="https://github.com/ViktoriiaProtasova/weather-app"
+        target="_blank"
+      >
+        GitHub
+      </a>{' '}
+      and hosted on{' '}
+      <a href="https://weatherprovide.netlify.app/" target="_blank">
+        Netlify
+      </a>
     </div>
   );
 }
