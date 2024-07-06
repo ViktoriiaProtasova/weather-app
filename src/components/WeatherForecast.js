ad6ffe9e8938e6e296c2f43f13e251542f62d737
Viktoriia Protasova <viktorka.protasova@gmail.com>
2024-07-06 18:47:26 +0300

    Update WeatherForecast.js

--- src/components/WeatherForecast.js
+++ src/components/WeatherForecast.js
@@ -4,8 +4,8 @@ import './WeatherForecast.css';
 import WeatherForecastDay from './WeatherForecastDay';
 
 export default function WeatherForecast(props) {
-  let [loaded, setLoaded] = useState(false);
-  let [forecast, setForecast] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
 
   useEffect(() => {
     setLoaded(false);
@@ -17,10 +17,10 @@ export default function WeatherForecast(props) {
   }
 
   function load() {
-    let apiKey = '5f472b7acba333cd8a035ea85a0d4d4c';
-    let longitude = props.coordinates.lon;
-    let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+    const longitude = props.coordinates.lon;
+    const latitude = props.coordinates.lat;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
   }
