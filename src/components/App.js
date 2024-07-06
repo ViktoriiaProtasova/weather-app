2ae4d49a075073ff808b8122ec5180fd28839566
Viktoriia Protasova <viktorka.protasova@gmail.com>
2024-07-06 13:34:33 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Weather from './Weather';
 import Footer from './Footer/Footer';
+import CurrentCity from './CurrentCity';
+import { MagnifyingGlass } from 'react-loader-spinner';
 
 export default function App() {
+  const [currentCityWeather, setCurrentCityWeather] = useState(null);
+
+  useEffect(() => {
+    CurrentCity(setCurrentCityWeather);
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        {currentCityWeather ? (
+          <Weather data={currentCityWeather} />
+        ) : (
+          <div className="loaderWrapper">
+            <MagnifyingGlass
+              className="magnifyingGlass"
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="magnifying-glass-loading"
+              glassColor="#f2f2f1"
+              color="#48484a"
+            />
+          </div>
+        )}
         <Footer />
       </div>
     </div>
